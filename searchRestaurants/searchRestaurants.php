eea7f8837ddb694f120b056a5191d50611f3deb1
cps-interactive <32658935+cps-interactive@users.noreply.github.com>
2019-03-25 18:34:12 -0400

    Added more search queries for the restaurant page

---
+++ searchRestaurants/searchRestaurants.php
@@ -0,0 +1,24 @@
+<?php
+        require "dbutil.php";
+        $db = DbUtil::loginConnection();
+
+        $stmt = $db->stmt_init();
+
+        if($stmt->prepare("select * from restaurant where ambiance like ?") or die(mysqli_error($db))) {
+                $searchString = '%' . $_GET['searchRestaurants'] . '%';
+                $stmt->bind_param(s, $searchString);
+                $stmt->execute();
+                $stmt->bind_result($placeID, $ambiance, $cuisine, $price, $menu);
+                echo "<table border=1><th>Place ID</th><th>Ambiance</th><th>Cuisine</th><th>Price</th><th>Menu</th>\n";
+                while($stmt->fetch()) {
+                        echo "<tr><td>$placeID</td><td>$ambiance</td><td>$cuisine</td><td>$price</td><td>$menu</td></tr>";
+                }
+                echo "</table>";
+
+                $stmt->close();
+        }
+
+        $db->close();
+
+
+?>
