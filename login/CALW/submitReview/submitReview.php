04e5055ed6a2abb1b20b6380b630e3ef42ae3342
Ross <ram8yx@virginia.edu>
2019-04-23 22:52:22 -0400

    we use store procedure in code yay

--- login/CALW/submitReview/submitReview.php
+++ login/CALW/submitReview/submitReview.php
@@ -4,7 +4,7 @@
 
         $stmt = $db->stmt_init();
 
-	if($stmt->prepare("INSERT INTO place_review (placeID, review) VALUES (?,?)") or die(mysqli_error($db))) {
+	if($stmt->prepare("call insertReview(?,?)") or die(mysqli_error($db))) {
                 $stmt->bind_param(ss, $ref, $aReview);
 		$ref = $_GET['submitR'];
                 $aReview = $_GET['actualR'];
